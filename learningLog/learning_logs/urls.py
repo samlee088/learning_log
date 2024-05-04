40d826a1ca2f5fa2f060bbacd52b4e61e6b8ead9
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-05-04 15:50:32 -0700

    single topic url, and single topic view function created

--- learningLog/learning_logs/urls.py
+++ learningLog/learning_logs/urls.py
@@ -8,6 +8,11 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
+
     # Page that shows the topics
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
 ]
