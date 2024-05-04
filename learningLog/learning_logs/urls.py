15e99ddadb816c7dbac82986ffe16836f72ce09c
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-05-04 15:38:23 -0700

    topics url, views created

--- learningLog/learning_logs/urls.py
+++ learningLog/learning_logs/urls.py
@@ -8,4 +8,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
+    # Page that shows the topics
+    path('/topics', views.topics, name='topics')
 ]
