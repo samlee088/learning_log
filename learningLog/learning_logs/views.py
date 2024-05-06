2afd11f0707c0122490dcee6209caade8a1ceace
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-05-06 16:55:35 -0700

    restricting pages to prevent user from viewing pages without logging, including topics, and entry

--- learningLog/learning_logs/views.py
+++ learningLog/learning_logs/views.py
@@ -10,11 +10,13 @@ def index(request):
     """The home page for the Learning Log"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
@@ -40,6 +42,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -60,6 +63,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
